0f6d6f72fc48648a522df87e2ec64427fdb0f766
Ritu Raj <rajritu1729@gmail.com>
2023-06-24 15:38:25 +0530

    first commit

---
+++ src/Image.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import axios from 'axios'
+import { useState } from 'react'
+
+const Image = () => {
+
+    const [images,setImages] = useState([]);
+    const [uploadedUrls , setUploadedUrls] = useState([]);
+
+  
+    const upload = async () => {
+        try{
+            if(images.length===0) return;
+
+            const p = images.map((image)=>{
+                const formData = new FormData();
+                formData.append('image',image);
+
+                return axios.post('https://api.imgbb.com/1/upload?key=d309c44cb2231f7de56d7e15c8de2897',formData);
+            })
+            const responses = await Promise.all(p);
+            const urls = responses.map((response)=>response.data.data.url);
+            setUploadedUrls((prevUrls)=> [...prevUrls,...urls]);
+            setImages([]);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    const change = async(e)=>{
+        const files = Array.from(e.target.files);
+        setImages((prevImages)=> [...prevImages,...files]);
+    };
+
+    const handleImageDelete = async (imageUrl) => {
+        try {
+          await axios.delete(`https://api.imgbb.com/1/image?key=d309c44cb2231f7de56d7e15c8de2897&image=${imageUrl}`);
+          setUploadedUrls((prevUrls) => prevUrls.filter((url) => url !== imageUrl));
+        } catch (error) {
+          console.error(error);
+        }
+      };
+
+
+  return (
+    <>
+        <input type='file' onChange={change} />
+        <button className='upload' onClick={upload}>Upload</button>
+        {uploadedUrls.length>0 && (
+            <div>
+                <h2>Uploaded Images:</h2>
+                {uploadedUrls.map((imageUrl,index)=>(
+                    <div key={index}>
+                        <img src={imageUrl} alt={`uploaded ${index}`}/>
+                        <button onClick={() => handleImageDelete(imageUrl)}>Delete</button>
+                        </div>
+                ))}
+                
+            </div>
+        )}
+    </>
+  )
+}
+
+export default Image
